0660453ae10f3aa490e42dde1c1e57c3cbc7daf5
HarshilShah1804 <hkshah1978@gmail.com>
2026-04-04 04:45:39 +0530

    feat: add AST parser

--- compile.sh
+++ compile.sh
@@ -2,11 +2,11 @@ set -e
 set -x
 
 # Compile the project
-cd ./lexer
+cd ./parser
 make clean && make
 
-cd ../parser
+cd ../lexer
 make clean && make
 cd ../
 
-gcc lexer/lex.yy.c parser/y.tab.c parser/derivation_tree.c -o parser_out
+gcc lexer/lex.yy.c parser/y.tab.c ast/ast.c ast/ast_builders.c ast/ast_utils.c ast/ast_print.c -o parser_out
