8b2781da43979319b7b1797c20983594aa1e8d71
HarshilShah1804 <hkshah1978@gmail.com>
2026-04-19 00:01:30 +0530

    update compilation script

--- compile.sh
+++ compile.sh
@@ -10,4 +10,4 @@ make clean && make
 cd ../
 
 # gcc lexer/lex.yy.c parser/y.tab.c ast/ast.c ast/ast_builders.c ast/ast_utils.c ast/ast_print.c -o parser_out
-gcc lexer/lex.yy.c parser/y.tab.c parser/derivation_tree.c -o parser_out
+gcc lexer/lex.yy.c parser/y.tab.c parser/derivation_tree.c ir/tac.c semantic/sdt_semantic.c -o parser_out
